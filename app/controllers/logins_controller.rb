e3967ecad475b2d23a35619865a8b8fa0a95626a
maysam <kherad@gmail.com>
2018-09-30 23:38:24 +0330

    logging heroku

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -14,7 +14,14 @@ class LoginsController < ApplicationController
   private
     def authenticate_with_google
       if flash[:google_sign_in_token].present?
-        User.find_by google_id: GoogleSignIn::Identity.new(flash[:google_sign_in_token]).user_id
+        iden = GoogleSignIn::Identity.new flash[:google_sign_in_token]
+        Rails.logger.error flash[:google_sign_in_token]
+        Rails.logger.error iden
+        user = User.find_by_google_id iden.user_id
+        unless user
+          user = User.create google_id: iden.user_id
+        end
+        user
       end
     end
 end
