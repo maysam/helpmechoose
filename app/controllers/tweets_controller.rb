2e5018ca823844e99361ccc34664e5fe3ae03d43
maysam <kherad@gmail.com>
2018-10-07 23:03:14 +0330

    showing tweets to all

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,7 +9,7 @@ class TweetsController < ApplicationController
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order id: :desc
   end
 
   # GET /tweets/1
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
+
+  def to_s
+    if name
+      name
+    elsif given_name && family_name
+      "#{given_name} #{family_name}"
+    else
+      email
+    end
+  end
 end
